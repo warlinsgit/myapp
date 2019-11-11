10d3519583ffd8bb86c1ff3d5e70ecddf3de7191
warlinsgit <warlins25@gmail.com>
2019-11-11 15:19:01 +0000

    initial author routes

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ const express = require('express');
 const app = express();
 
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 
 app.set('view engine', 'ejs');
@@ -18,9 +20,12 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true,
+useNewUrlParser: true,
+useFindAndModify: true, })
 
 const db = mongoose.connection;
 db.on('error', error => console.log(error))
@@ -28,5 +33,6 @@ db.once('open', () => console.log('connected to mongoose'))
 
 
 app.use('/', indexRouter);
+app.use('/authors',  authorRouter);
 
 app.listen(process.env.PORT || 3000);
